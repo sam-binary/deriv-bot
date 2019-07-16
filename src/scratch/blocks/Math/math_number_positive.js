d146606d741204454d7bf786a924d18afbc660e8
McSam <samyong@Sams-Mac.local>
2019-07-16 15:40:32 +0800

    fix bug

--- src/scratch/blocks/Math/math_number_positive.js
+++ src/scratch/blocks/Math/math_number_positive.js
@@ -1,5 +1,32 @@
+import { translate } from '../../../utils/lang/i18n';
+
 Blockly.Blocks.math_number_positive = {
     init: Blockly.Blocks.math_number.init,
+    definition(){
+        return {
+            message0: '%1',
+            args0   : [
+                {
+                    type : 'field_number',
+                    name : 'NUM',
+                    value: 0,
+                },
+            ],
+            output         : 'Number',
+            outputShape    : Blockly.OUTPUT_SHAPE_ROUND,
+            colour         : '#dedede',
+            colourSecondary: '#ffffff',
+            colourTertiary : '#ffffff',
+            tooltip        : translate('Math Number Tooltip'),
+            category       : 'mathematical',
+        };
+    },
+    meta() {
+        return {
+            'display_name': translate('Math Number Positive'),
+            'description' : translate('Math Number Description'),
+        };
+    },
     numberValidator(input) {
         if (/^([0][,.]|[1-9]+[,.])?([0]|[1-9])*$/.test(input)) {
             return undefined;
